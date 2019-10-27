f57c18787e199ead0914707151ef139fff464566
cookiedragon234 <c.o.okiedragon234@gmail.com>
2019-10-27 21:35:01 +0000

    Add pulldown

---
+++ src/main/java/me/rigamortis/seppuku/impl/module/movement/PullDownModule.java
@@ -0,0 +1,51 @@
+package me.rigamortis.seppuku.impl.module.movement;
+
+import me.rigamortis.seppuku.api.event.EventStageable;
+import me.rigamortis.seppuku.api.event.player.EventPlayerUpdate;
+import me.rigamortis.seppuku.api.module.Module;
+import me.rigamortis.seppuku.api.value.NumberValue;
+import net.minecraft.client.Minecraft;
+import team.stiff.pomelo.impl.annotated.handler.annotation.Listener;
+
+/**
+ * @author cookiedragon234
+ * (cookiedragon234 owns me and all)
+ * also buy backdoored client kk thx
+ */
+public class PullDownModule extends Module
+{
+	public PullDownModule()
+	{
+		super("PullDown", new String[]{"FastFall"}, "Increase your downwards velocity when falling", "NONE", -1, ModuleType.MOVEMENT);
+	}
+	
+	public final NumberValue<Float> speed = new NumberValue<>("Speed", new String[]{"velocity"}, 10f, Float.class, 0f, 20f, 1f);
+	
+	@Listener
+	public void onUpdate(EventPlayerUpdate event)
+	{
+		if (event.getStage() == EventStageable.EventStage.PRE)
+		{
+			final Minecraft mc = Minecraft.getMinecraft();
+			
+			// obvs dont do this when flying or when using elytras
+			if(mc.player.isElytraFlying() || mc.player.capabilities.isFlying || mc.player.onGround) return;
+			
+			// dont trigger when they could just be jumping, 3 blocks is maybe overkill? But its probably the best thing to do
+			boolean isBlockBelow =
+				!mc.world.isAirBlock(mc.player.getPosition().add(0, -1, 0))
+				||
+				!mc.world.isAirBlock(mc.player.getPosition().add(0, -2, 0))
+				||
+				!mc.world.isAirBlock(mc.player.getPosition().add(0, -3, 0))
+			;
+			
+			if(!isBlockBelow)
+			{
+				// Pull the player down
+				mc.player.motionY = -(speed.getFloat());
+				// kk thx that ends epic module bsb on top
+			}
+		}
+	}
+}
