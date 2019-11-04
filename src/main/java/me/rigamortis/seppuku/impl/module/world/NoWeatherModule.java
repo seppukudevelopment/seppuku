8f12f80515544c11583f787af1d15d34f8bfd6a7
noil <robloxguy@protonmail.com>
2019-11-04 13:48:45 -0500

    Remove un-used options from NoWeather

--- src/main/java/me/rigamortis/seppuku/impl/module/world/NoWeatherModule.java
+++ src/main/java/me/rigamortis/seppuku/impl/module/world/NoWeatherModule.java
@@ -2,7 +2,6 @@ package me.rigamortis.seppuku.impl.module.world;
 
 import me.rigamortis.seppuku.api.event.world.EventRainStrength;
 import me.rigamortis.seppuku.api.module.Module;
-import me.rigamortis.seppuku.api.value.OptionalValue;
 import team.stiff.pomelo.impl.annotated.handler.annotation.Listener;
 
 /**
@@ -11,15 +10,8 @@ import team.stiff.pomelo.impl.annotated.handler.annotation.Listener;
  */
 public final class NoWeatherModule extends Module {
 
-    public final OptionalValue mode = new OptionalValue("Mode", new String[]{"Mode", "M"}, 0, new String[]{"Remove", "Rain"});
-
     public NoWeatherModule() {
         super("NoWeather", new String[]{"AntiWeather"}, "Allows you to control the weather client-side", "NONE", -1, ModuleType.WORLD);
-    }
-
-    @Override
-    public String getMetaData() {
-        return this.mode.getSelectedOption();
     }
 
     @Listener
