d4a147e6e59bb617236ce3d8190af6dd73f92025
noil <robloxguy@protonmail.com>
2020-11-25 17:59:57 -0500

    Clamps tooltips to stay inside screen boundary

--- src/main/java/me/rigamortis/seppuku/api/gui/hud/component/ToolTipComponent.java
+++ src/main/java/me/rigamortis/seppuku/api/gui/hud/component/ToolTipComponent.java
@@ -3,6 +3,7 @@ package me.rigamortis.seppuku.api.gui.hud.component;
 import me.rigamortis.seppuku.api.util.ColorUtil;
 import me.rigamortis.seppuku.api.util.RenderUtil;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
 
 /**
@@ -35,6 +36,9 @@ public class ToolTipComponent extends HudComponent {
             this.setX(mouseX - (this.getW() / 2.0f));
             this.setY(mouseY - 12);
 
+            // clamp tooltip to stay inside the screen bounds
+            this.clamp();
+
             // background
             RenderUtil.drawRect(this.getX() - 2, this.getY() - 2, this.getX() + this.getW() + 2, this.getY() + this.getH() + 2, ColorUtil.changeAlpha(0x80101010, this.alpha / 2));
             RenderUtil.drawRect(this.getX() - 1, this.getY() - 1, this.getX() + this.getW() + 1, this.getY() + this.getH() + 1, ColorUtil.changeAlpha(0xAD101010, this.alpha / 2));
@@ -43,6 +47,26 @@ public class ToolTipComponent extends HudComponent {
             GlStateManager.enableBlend();
             Minecraft.getMinecraft().fontRenderer.drawStringWithShadow(this.text, this.getX() + 1, this.getY() + 1, ColorUtil.changeAlpha(0xADC255FF, this.alpha));
             GlStateManager.disableBlend();
+        }
+    }
+
+    private void clamp() {
+        final ScaledResolution sr = new ScaledResolution(Minecraft.getMinecraft());
+
+        if (this.getX() <= 0) {
+            this.setX(2);
+        }
+
+        if (this.getY() <= 0) {
+            this.setY(2);
+        }
+
+        if (this.getX() + this.getW() >= sr.getScaledWidth() - 2) {
+            this.setX(sr.getScaledWidth() - 2 - this.getW());
+        }
+
+        if (this.getY() + this.getH() >= sr.getScaledHeight() - 2) {
+            this.setY(sr.getScaledHeight() - 2 - this.getH());
         }
     }
 }
