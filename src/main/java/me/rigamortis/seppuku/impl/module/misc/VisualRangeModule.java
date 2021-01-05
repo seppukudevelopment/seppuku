3901469a17a7108bae067dcb00495be280636e49
noil <robloxguy@protonmail.com>
2021-01-04 20:56:44 -0500

    VisualRange: Adds mode, ELytraFly: Lowers autostart delay

--- src/main/java/me/rigamortis/seppuku/impl/module/misc/VisualRangeModule.java
+++ src/main/java/me/rigamortis/seppuku/impl/module/misc/VisualRangeModule.java
@@ -6,6 +6,7 @@ import me.rigamortis.seppuku.api.event.world.EventAddEntity;
 import me.rigamortis.seppuku.api.event.world.EventRemoveEntity;
 import me.rigamortis.seppuku.api.friend.Friend;
 import me.rigamortis.seppuku.api.module.Module;
+import me.rigamortis.seppuku.api.value.Value;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import team.stiff.pomelo.impl.annotated.handler.annotation.Listener;
@@ -15,6 +16,12 @@ import team.stiff.pomelo.impl.annotated.handler.annotation.Listener;
  * 11/10/2019 @ 4:20 AM.
  */
 public final class VisualRangeModule extends Module {
+
+    public final Value<Mode> mode = new Value<Mode>("Mode", new String[]{"Mode", "M"}, "Change between alert modes.", Mode.NOTIFICATION);
+
+    private enum Mode {
+        CHAT, NOTIFICATION, BOTH
+    }
 
     private int prevPlayer = -1;
 
@@ -27,8 +34,15 @@ public final class VisualRangeModule extends Module {
         if (Minecraft.getMinecraft().world != null && !Minecraft.getMinecraft().player.isDead && event.getEntity() instanceof EntityPlayer && !event.getEntity().getName().equalsIgnoreCase(Minecraft.getMinecraft().player.getName())) {
             final Friend friend = Seppuku.INSTANCE.getFriendManager().isFriend(event.getEntity());
             final String msg = (friend != null ? ChatFormatting.DARK_PURPLE : ChatFormatting.RED) + (friend != null ? friend.getAlias() : event.getEntity().getName()) + ChatFormatting.WHITE + " has entered your visual range.";
-            Seppuku.INSTANCE.getNotificationManager().addNotification("", msg);
-            Seppuku.INSTANCE.logChat(msg);
+
+            if (this.mode.getValue() == Mode.NOTIFICATION || this.mode.getValue() == Mode.BOTH) {
+                Seppuku.INSTANCE.getNotificationManager().addNotification("", msg);
+            }
+
+            if (this.mode.getValue() == Mode.CHAT || this.mode.getValue() == Mode.BOTH) {
+                Seppuku.INSTANCE.logChat(msg);
+            }
+
             if (event.getEntity().getEntityId() == this.prevPlayer) {
                 this.prevPlayer = -1;
             }
@@ -42,8 +56,14 @@ public final class VisualRangeModule extends Module {
                 this.prevPlayer = event.getEntity().getEntityId();
                 final Friend friend = Seppuku.INSTANCE.getFriendManager().isFriend(event.getEntity());
                 final String msg = (friend != null ? ChatFormatting.DARK_PURPLE : ChatFormatting.RED) + (friend != null ? friend.getAlias() : event.getEntity().getName()) + ChatFormatting.WHITE + " has left your visual range.";
-                Seppuku.INSTANCE.getNotificationManager().addNotification("", msg);
-                Seppuku.INSTANCE.logChat(msg);
+
+                if (this.mode.getValue() == Mode.NOTIFICATION || this.mode.getValue() == Mode.BOTH) {
+                    Seppuku.INSTANCE.getNotificationManager().addNotification("", msg);
+                }
+
+                if (this.mode.getValue() == Mode.CHAT || this.mode.getValue() == Mode.BOTH) {
+                    Seppuku.INSTANCE.logChat(msg);
+                }
             }
         }
     }
