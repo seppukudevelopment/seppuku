82b06b314a1397dcc8cbba6fc2ea12933508b8af
noil <robloxguy@protonmail.com>
2021-01-10 00:26:46 -0500

    NoteBot: Updates

--- src/main/java/me/rigamortis/seppuku/impl/module/world/NoteBotModule.java
+++ src/main/java/me/rigamortis/seppuku/impl/module/world/NoteBotModule.java
@@ -33,10 +33,11 @@ import java.util.stream.IntStream;
 public final class NoteBotModule extends Module {
 
     private final Value<BotState> state = new Value<BotState>("State", new String[]{"State", "s"}, "Current state of the note-bot.", BotState.IDLE);
-    private final Value<Mode> mode = new Value<Mode>("Mode", new String[]{"mod", "m"}, "Current mode of the note-bot.", Mode.AUTOMATIC);
+    private final Value<Mode> mode = new Value<Mode>("Mode", new String[]{"mod", "m"}, "Current mode of the note-bot.", Mode.NORMAL);
     private final Value<Boolean> rotate = new Value<Boolean>("Rotate", new String[]{"rot", "r"}, "Rotate the player's head & body for each note-bot function.", true);
     private final Value<Boolean> swing = new Value<Boolean>("Swing", new String[]{"swingarm", "armswing", "sa"}, "Swing the player's hand for each note-bot function.", true);
-    private final Value<Float> clickDelay = new Value<Float>("ClickDelay", new String[]{"Click Delay", "click-delay", "delay", "del", "cd", "d"}, "Delay(ms) to wait between clicks.", 200.0f, 0.0f, 1000.0f, 1.0f);
+    private final Value<Float> discoverDelay = new Value<Float>("DiscoverDelay", new String[]{"Discover Delay", "discover-delay", "ddelay", "ddel", "dd", "d"}, "Delay(ms) to wait between left clicks.", 50.0f, 0.0f, 1000.0f, 1.0f);
+    private final Value<Float> tuneDelay = new Value<Float>("TuneDelay", new String[]{"Tune Delay", "tune-delay", "tdelay", "tdel", "td"}, "Delay(ms) to wait between right clicks.", 200.0f, 0.0f, 1000.0f, 1.0f);
 
     private final RotationTask rotationTask = new RotationTask("NoteBot", 2);
 
@@ -46,7 +47,9 @@ public final class NoteBotModule extends Module {
     private final int[] positionOffsets = new int[]{2, 1, 2};
 
     private final NotePlayer notePlayer = new NotePlayer();
-    private final Timer timer = new Timer();
+    private final Timer discoverTimer = new Timer();
+    private final Timer tuneTimer = new Timer();
+    private final Timer stateTimer = new Timer();
 
     private final List<BlockPos> blocks = new ArrayList<>();
     private final List<BlockPos> tunedBlocks = new ArrayList<>();
@@ -71,7 +74,7 @@ public final class NoteBotModule extends Module {
             this.blocks.add(new BlockPos(area[0], area[1], area[2]));
         });
 
-        if (this.mode.getValue().equals(Mode.AUTOMATIC)) {
+        if (this.mode.getValue().equals(Mode.NORMAL)) {
             this.state.setEnumValue("DISCOVERING");
         }
     }
@@ -112,11 +115,19 @@ public final class NoteBotModule extends Module {
         BlockPos position = packetBlockAction.getBlockPosition();
         this.blocks.stream().filter(blockPos -> this.correctPosition(position, this.blocks.indexOf(blockPos))).forEach(blockPos -> {
             final Note note = new Note(this.blocks.indexOf(blockPos), position, packetBlockAction.getData1(), packetBlockAction.getData2());
-            if (!this.discoveredBlocks.containsKey(blockPos)) {
-                this.discoveredBlocks.put(blockPos, note);
-            } else {
-                if (!this.tunedBlocks.contains(blockPos) && this.blocks.indexOf(blockPos) == packetBlockAction.getData2()) {
-                    this.tunedBlocks.add(blockPos);
+            this.discoveredBlocks.put(blockPos, note);
+
+            if (!this.tunedBlocks.contains(blockPos) && this.blocks.indexOf(blockPos) == packetBlockAction.getData2()) {
+                this.tunedBlocks.add(blockPos);
+            }
+
+            if (!this.mode.getValue().equals(Mode.DEBUG) && !this.state.getValue().equals(BotState.PLAYING)) {
+                if (!this.state.getValue().equals(BotState.TUNING)) {
+                    if (this.discoveredBlocks.size() != BLOCK_AREA) {
+                        this.stateTimer.reset();
+                    } else if (this.tunedBlocks.size() == BLOCK_AREA) {
+                        this.state.setValue(BotState.IDLE);
+                    }
                 }
             }
         });
@@ -144,31 +155,36 @@ public final class NoteBotModule extends Module {
                     }
                 }
 
-                if (this.mode.getValue().equals(Mode.AUTOMATIC)) {
-                    if ((this.discoveredBlocks.size() == BLOCK_AREA && this.tunedBlocks.size() == BLOCK_AREA)) {
-                        this.state.setEnumValue("IDLE");
+                if (this.mode.getValue().equals(Mode.NORMAL)) {
+                    if (this.state.getValue().equals(BotState.PLAYING)) {
+                        if (this.getNotePlayer().getNotesToPlay().size() <= 0) {
+                            //this.state.setValue(BotState.IDLE);
+                        }
+                    }
+
+                    if (this.state.getValue().equals(BotState.TUNING)) {
+                        if (this.discoveredBlocks.size() == BLOCK_AREA && this.tunedBlocks.size() == BLOCK_AREA) {
+                            this.state.setValue(BotState.IDLE);
+                        }
                     }
                 }
 
-                if (this.currentBlock == null) {
+                if (this.currentBlock == null && !this.state.getValue().equals(BotState.PLAYING)) {
                     this.blocks.stream().filter(blockPos -> (!this.discoveredBlocks.containsKey(blockPos) || !this.tunedBlocks.contains(blockPos))).forEach(blockPos -> {
                         final BlockPos workPos = new BlockPos(this.getPosition(this.blocks.indexOf(blockPos)));
-
                         if (!this.discoveredBlocks.containsKey(blockPos)) {
-                            if (this.mode.getValue().equals(Mode.AUTOMATIC)) {
-                                this.state.setEnumValue("DISCOVERING");
+                            if (!this.mode.getValue().equals(Mode.DEBUG)) {
+                                this.state.setValue(BotState.DISCOVERING);
                             }
-                            this.currentBlock = new BlockPos(workPos);
-                            if (this.rotate.getValue()) {
-                                this.lookAtPosition(workPos);
-                            }
-                        } else if (this.discoveredBlocks.size() == BLOCK_AREA) {
-                            if (this.mode.getValue().equals(Mode.AUTOMATIC)) {
-                                this.state.setEnumValue("TUNING");
-                            }
-                            this.currentBlock = new BlockPos(workPos);
-                            if (this.rotate.getValue()) {
-                                this.lookAtPosition(workPos);
+                            this.setCurrentNoteBlock(workPos);
+                        } else if (!this.tunedBlocks.contains(workPos)) {
+                            if (!this.mode.getValue().equals(Mode.DEBUG)) {
+                                if (this.stateTimer.passed(1000)) {
+                                    this.state.setValue(BotState.TUNING);
+                                    this.setCurrentNoteBlock(workPos);
+                                }
+                            } else {
+                                this.setCurrentNoteBlock(workPos);
                             }
                         }
                     });
@@ -183,26 +199,36 @@ public final class NoteBotModule extends Module {
                             if (this.rotationTask.isOnline()) {
                                 Seppuku.INSTANCE.getRotationManager().finishTask(this.rotationTask);
                             }
-                            return;
+                            break;
                         case DISCOVERING:
                             if (this.discoveredBlocks.size() != BLOCK_AREA) {
-                                mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.START_DESTROY_BLOCK, this.currentBlock, direction));
-                                if (this.swing.getValue()) {
-                                    mc.player.swingArm(EnumHand.MAIN_HAND);
+                                if (this.discoverTimer.passed(this.discoverDelay.getValue())) {
+                                    mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.START_DESTROY_BLOCK, this.currentBlock, direction));
+                                    mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.ABORT_DESTROY_BLOCK, this.currentBlock, direction));
+                                    if (this.swing.getValue()) {
+                                        mc.player.swingArm(EnumHand.MAIN_HAND);
+                                    }
+                                    this.discoveredBlocks.put(this.currentBlock, new Note(this.blocks.indexOf(this.currentBlock), this.currentBlock, 0, this.blocks.indexOf(this.currentBlock)));
+                                    this.currentBlock = null;
+                                    this.discoverTimer.reset();
                                 }
-                                mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.ABORT_DESTROY_BLOCK, this.currentBlock, direction));
-                                this.currentBlock = null;
                             }
                             break;
                         case TUNING:
+                            if (!this.mode.getValue().equals(Mode.DEBUG)) {
+                                if (this.discoveredBlocks.size() != BLOCK_AREA) {
+                                    this.state.setValue(BotState.DISCOVERING);
+                                    break;
+                                }
+                            }
                             if (this.tunedBlocks.size() != BLOCK_AREA && !this.tunedBlocks.contains(this.currentBlock)) {
-                                if (this.timer.passed(this.clickDelay.getValue())) {
+                                if (this.tuneTimer.passed(this.tuneDelay.getValue())) {
                                     mc.playerController.processRightClickBlock(mc.player, mc.world, this.currentBlock, direction, new Vec3d(0.5F, 0.5F, 0.5F), EnumHand.MAIN_HAND);
                                     if (this.swing.getValue()) {
                                         mc.player.swingArm(EnumHand.MAIN_HAND);
                                     }
                                     this.currentBlock = null;
-                                    this.timer.reset();
+                                    this.tuneTimer.reset();
                                 }
                             } else {
                                 this.currentBlock = null;
@@ -216,10 +242,10 @@ public final class NoteBotModule extends Module {
                             this.currentNote++;
                             if (this.currentNote != -1) {
                                 mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.START_DESTROY_BLOCK, this.currentBlock, direction));
+                                mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.ABORT_DESTROY_BLOCK, this.currentBlock, direction));
                                 if (this.swing.getValue()) {
                                     mc.player.swingArm(EnumHand.MAIN_HAND);
                                 }
-                                mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.ABORT_DESTROY_BLOCK, this.currentBlock, direction));
                                 this.currentBlock = null;
                             }
                             break;
@@ -254,37 +280,11 @@ public final class NoteBotModule extends Module {
                 .getZ() == blocks[2]);
     }
 
-    private String getNote(int note) {
-        int octaveNote = note % 12;
-        switch (octaveNote) {
-            case 0:
-                return "F#";
-            case 1:
-                return "G";
-            case 2:
-                return "G#";
-            case 3:
-                return "A";
-            case 4:
-                return "A#";
-            case 5:
-                return "B";
-            case 6:
-                return "C";
-            case 7:
-                return "C#";
-            case 8:
-                return "D";
-            case 9:
-                return "D#";
-            case 10:
-                return "E";
-            case 11:
-                return "F";
-            case 12:
-                return "Gb";
+    private void setCurrentNoteBlock(BlockPos pos) {
+        this.currentBlock = new BlockPos(pos);
+        if (this.rotate.getValue()) {
+            this.lookAtPosition(pos);
         }
-        return "null";
     }
 
     private void clearData() {
@@ -302,7 +302,7 @@ public final class NoteBotModule extends Module {
     }
 
     public enum Mode {
-        AUTOMATIC, MANUAL, DEBUG
+        NORMAL, DEBUG
     }
 
     public Value<BotState> getState() {
