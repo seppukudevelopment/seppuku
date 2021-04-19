74aa3173cbfe1c04d05c0d04761d34673dab7d58
noil <robloxguy@protonmail.com>
2021-04-19 14:39:51 -0400

    Redesigned BiomeComponent

--- src/main/java/me/rigamortis/seppuku/impl/gui/hud/component/BiomeComponent.java
+++ src/main/java/me/rigamortis/seppuku/impl/gui/hud/component/BiomeComponent.java
@@ -1,5 +1,6 @@
 package me.rigamortis.seppuku.impl.gui.hud.component;
 
+import com.mojang.realmsclient.gui.ChatFormatting;
 import me.rigamortis.seppuku.api.gui.hud.component.DraggableHudComponent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.biome.Biome;
@@ -24,9 +25,9 @@ public final class BiomeComponent extends DraggableHudComponent {
             final BlockPos pos = mc.player.getPosition();
             final Chunk chunk = mc.world.getChunk(pos);
             final Biome biome = chunk.getBiome(pos, mc.world.getBiomeProvider());
-
-            this.setW(mc.fontRenderer.getStringWidth(biome.getBiomeName()));
-            mc.fontRenderer.drawStringWithShadow(biome.getBiomeName(), this.getX(), this.getY(), -1);
+            final String text = ChatFormatting.GRAY + "Biome" + ChatFormatting.RESET + " " + biome.getBiomeName();
+            this.setW(mc.fontRenderer.getStringWidth(text));
+            mc.fontRenderer.drawStringWithShadow(text, this.getX(), this.getY(), -1);
         } else {
             this.setW(mc.fontRenderer.getStringWidth("(biome)"));
             mc.fontRenderer.drawStringWithShadow("(biome)", this.getX(), this.getY(), 0xFFAAAAAA);
