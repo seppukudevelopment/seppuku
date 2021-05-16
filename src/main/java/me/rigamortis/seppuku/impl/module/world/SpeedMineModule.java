ee7015b39a2d6b3183178f6f2bc83b6c397c7017
noil <robloxguy@protonmail.com>
2021-05-16 14:30:48 -0400

    SpeedMine cleanup & added StringUtil function

--- src/main/java/me/rigamortis/seppuku/impl/module/world/SpeedMineModule.java
+++ src/main/java/me/rigamortis/seppuku/impl/module/world/SpeedMineModule.java
@@ -29,22 +29,21 @@ import team.stiff.pomelo.impl.annotated.handler.annotation.Listener;
  */
 public final class SpeedMineModule extends Module {
 
-    public final Value<Mode> mode = new Value<Mode>("Mode", new String[]{"Mode", "M"}, "The speed-mine mode to use.", Mode.PACKET);
+    public final Value<Mode> mode = new Value<Mode>("Mode", new String[]{"Mode", "M"}, "The speed-mine mode to use.", Mode.DAMAGE);
 
     private enum Mode {
         PACKET, DAMAGE, INSTANT, SEQUENTIAL
     }
 
-    public BlockPos seqPos;
     public static BlockPos autoPos;
+
+    public BlockPos seqPos;
     public EnumFacing seqDir;
-    final Minecraft mc = Minecraft.getMinecraft();
+    public final Minecraft mc = Minecraft.getMinecraft();
 
     public final Value<Boolean> reset = new Value<Boolean>("Reset", new String[]{"Res"}, "Stops current block destroy damage from resetting if enabled.", true);
     public final Value<Boolean> doubleBreak = new Value<Boolean>("DoubleBreak", new String[]{"DoubleBreak", "Double", "DB"}, "Mining a block will also mine the block above it, if enabled.", false);
-    public final Value<Boolean> auto = new Value<Boolean>("Auto", new String[]{"Res"}, "Auto sets the current mode to allow for multimining", true);
-
-
+    public final Value<Boolean> auto = new Value<Boolean>("Auto", new String[]{"Res"}, "When enabled, allows for multi-mining blocks.", false);
 
     public SpeedMineModule() {
         super("SpeedMine", new String[]{"FastMine"}, "Allows you to break blocks faster", "NONE", -1, ModuleType.WORLD);
