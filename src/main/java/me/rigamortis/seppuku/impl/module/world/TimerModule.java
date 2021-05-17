20b0b9c7e1068a25639802bd9f8917169833b1f9
noil <robloxguy@protonmail.com>
2021-05-17 13:22:38 -0400

    Timer updates

--- src/main/java/me/rigamortis/seppuku/impl/module/world/TimerModule.java
+++ src/main/java/me/rigamortis/seppuku/impl/module/world/TimerModule.java
@@ -15,10 +15,10 @@ import team.stiff.pomelo.impl.annotated.handler.annotation.Listener;
 public final class TimerModule extends Module {
 
     public final Value<Float> speed = new Value<Float>("Speed", new String[]{"Spd"}, "Tick-rate multiplier. [(20tps/second) * (this value)]", 4.0f, 0.0f, 10.0f, 0.1f);
-    public final Value<Boolean> tpsSync = new Value<Boolean>("TPSSync", new String[]{"Sync"}, "Syncs timer with the servers TPS", false);
+    public final Value<Boolean> tpsSync = new Value<Boolean>("TPSSync", new String[]{"TPS", "Sync"}, "Syncs timer with the servers TPS", false);
 
     public TimerModule() {
-        super("Timer", new String[]{"Time", "Tmr"}, "Speeds up the client tick rate", "NONE", -1, ModuleType.WORLD);
+        super("Timer", new String[]{"Time", "Tmr"}, "Speeds up the client tick rate.", "NONE", -1, ModuleType.WORLD);
     }
 
     @Override
@@ -29,16 +29,16 @@ public final class TimerModule extends Module {
 
     @Override
     public String getMetaData() {
-        return "" + this.speed.getValue();
+        return "" + String.format("%.2f", this.speed.getValue());
     }
 
     @Listener
     public void onUpdate(EventPlayerUpdate event) {
         if (event.getStage() == EventStageable.EventStage.PRE) {
-            if (tpsSync.getValue()) {
-                speed.setValue(Seppuku.INSTANCE.getTickRateManager().getTickRate()/20);
+            if (this.tpsSync.getValue()) {
+                this.speed.setValue(Seppuku.INSTANCE.getTickRateManager().getTickRate() / 20.0f);
             }
-            Minecraft.getMinecraft().timer.tickLength = 50.0f / speed.getValue();
+            Minecraft.getMinecraft().timer.tickLength = 50.0f / this.speed.getValue();
         }
     }
 
